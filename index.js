ad256b4195d3c6195ed9590b5d8f76bd4995b4e4
Vinayak S <vinayaksukhalal@gmail.com>
2024-10-19 20:22:25 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
 var bodyparser = require("body-parser");
-
+const cors = require('cors');
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 var main = require("./main");
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
+app.use(cors());
 // Serve static files from the 'public' directory
 app.use(express.static("public"));
 
